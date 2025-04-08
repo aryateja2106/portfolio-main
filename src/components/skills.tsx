7b7d4d57d3244a412d3b269cd6a0a0f05527ae93
Ganesh Alla <allaganesh03@outlook.com>
2025-04-09 00:05:06 +0530

    test

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -61,7 +61,7 @@ export const TechStack = () => {
   // Function to activate a category
   const handleCategoryClick = (category: string) => {
     setActiveCategory(category);
-    // Increment the key to force re-render of TechGravity
+    // Increment the key to force re-render of TechGravity.
     setGravityKey(prev => prev + 1);
   };
 
